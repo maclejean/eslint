ffc826cfbf6fa44680da4b410a21d005a403fed4
张杰 <zhangjie@skybility.com>
2020-03-15 10:21:04 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,24 @@
 module.exports = {
-    "extends": "airbnb"
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "es7": true
+    },
+    "plugins": ["react"],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module",
+        "ecmaVersion": 7
+    },
+    "extends": ["airbnb", "plugin:prettier/recommended",
+        "plugin:react/recommended",
+    ],
+    "rules": {
+        "prettier/prettier": "error",
+        "react/jsx-filename-extension": ['error', { extensions: ['.js', '.jsx'] }],
+        "react/jsx-uses-react": 1,
+    }
 };
