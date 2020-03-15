7e66f0d58b5445d2df1e88c3d04824996e7f6599
张杰 <zhangjie@skybility.com>
2020-03-15 21:31:18 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,37 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "env": {
-        "browser": true,
-        "es7": true
+    env: {
+        browser: true,
+        es6: true,
+        commonjs: true,
     },
-    "plugins": ["react"],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
+    plugins: ['react', 'import', 'jsx-a11y', 'prettier', 'react-hooks'],
+    parserOptions: {
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+            jsx: true,
         },
-        "sourceType": "module",
-        "ecmaVersion": 7
+        sourceType: 'module',
+        ecmaVersion: 6,
     },
-    "extends": ["airbnb", "plugin:prettier/recommended",
-        "plugin:react/recommended",
+    extends: [
+        'airbnb',
+        'plugin:prettier/recommended',
+        'prettier/babel',
+        'plugin:react/recommended',
     ],
-    "rules": {
-        "prettier/prettier": "error",
-        "react/jsx-filename-extension": ['error', { extensions: ['.js', '.jsx'] }],
-        "react/jsx-uses-react": 1,
-    }
+    rules: {
+        'prettier/prettier': 'error',
+        'react/jsx-filename-extension': [
+            'error',
+            { extensions: ['.js', '.jsx'] },
+        ],
+        'no-use-before-define': 1,
+        'react/jsx-indent': 0,
+        'react/jsx-one-expression-per-line': 0,
+        'react/jsx-first-prop-new-line': 0,
+        'react/jsx-indent-props': 0,
+        'no-param-reassign': 1,
+        'react/jsx-closing-bracket-location': [1, 'after-props'],
+        'react/jsx-uses-react': 'error',
+    },
 };
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    parser: 'babel',
+    trailingComma: 'es5',
+    tabWidth: 4,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: true,
+    jsxBracketSameLine: true,
+    endOfLine: 'auto',
+};
